da1a45f8917e6a00a85f7e2dd14cf3832bc712da
Rami <ramiraza@Ramis-MacBook-Pro.local>
2019-08-05 17:00:08 -0700

    Updates

---
+++ precache-manifest.ce1739ae66d6c0d2b2d0e86fbce182b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff55940b1d15b755b67a0e7caa5a418b",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "c4c885e9c4e5d324ea45",
+    "url": "/monster-rolodex/static/css/main.9309f8b4.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "c4c885e9c4e5d324ea45",
+    "url": "/monster-rolodex/static/js/main.26e96ab7.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
